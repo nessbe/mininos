f531fd57943e8d27c51ace1a7359786ef43dccb7
Nessbe <felix.chaupite@gmail.com>
2026-02-23 00:26:50 +0100

    Scripts: Add run script

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+# scripts/run.sh
+#
+# Copyright (c) 2026-Present Nessbe
+#
+# This file is licensed under the terms specified in the
+# LICENSE file located at the root of this repository.
+
+#!/usr/bin/sh
+
+BUILD_DIR=build
+RUN_TARGET=run
+
+cmake --build "$BUILD_DIR" --target $RUN_TARGET
