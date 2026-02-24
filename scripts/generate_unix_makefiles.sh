7627e02817645a51a3a4268cdb2c8cc22a2c3b6c
Nessbe <felix.chaupite@gmail.com>
2026-02-24 14:45:46 +0100

    Scripts: Add Unix Makefiles generator script

---
+++ scripts/generate_unix_makefiles.sh
@@ -0,0 +1,10 @@
+# scripts/generate_unix_makefiles.sh
+#
+# Copyright (c) 2026-Present Nessbe
+#
+# This file is licensed under the terms specified in the
+# LICENSE file located at the root of this repository.
+
+#!/usr/bin/sh
+
+scripts/run_generator.sh "Unix Makefiles"
