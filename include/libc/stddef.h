f3f974f8f499dfb68373a3c621bfb7e0fa88c892
Nessbe <felix.chaupitre@gmail.com>
2026-03-06 18:55:44 +0100

    Libc: Add stddef.h

---
+++ include/libc/stddef.h
@@ -0,0 +1,15 @@
+// include/libc/stddef.h
+//
+// Copyright (c) 2026-Present Nessbe
+//
+// This file is licensed under the terms specified in the
+// LICENSE file located at the root of this repository.
+
+#ifndef STDDEF_H
+#define STDDEF_H
+
+#define NULL ((void *)0)
+
+typedef unsigned long int size_t;
+
+#endif
