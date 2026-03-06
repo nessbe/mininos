72fa3666a58bcf99113063dc3c6d1c133e5cd555
Nessbe <felix.chaupitre@gmail.com>
2026-03-06 18:55:58 +0100

    Libc: Add stdint.h

---
+++ include/libc/stdint.h
@@ -0,0 +1,25 @@
+// include/libc/stdint.h
+//
+// Copyright (c) 2026-Present Nessbe
+//
+// This file is licensed under the terms specified in the
+// LICENSE file located at the root of this repository.
+
+#ifndef STDINT_H
+#define STDINT_H
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+
+typedef signed short int int16_t;
+typedef unsigned short int uint16_t;
+
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+
+typedef signed long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+typedef unsigned long int uintptr_t;
+
+#endif
